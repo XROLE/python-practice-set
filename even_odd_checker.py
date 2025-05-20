f616a901e1e77b674748a4ad4fa8bc54180e2e12
xrole <xrolediamond@gmail.com>
2025-05-20 11:50:20 +0100

    solve even odd checker

---
+++ even_odd_checker.py
@@ -0,0 +1,16 @@
+# ✅ 1. Even or Odd Checker
+# Write a program that takes a number from the user and prints whether it's even or odd.
+# python
+# CopyEdit
+# Input: 4
+# Output: 4 is even
+
+# ===================================================
+# Solution
+# num = int(input("What is your magic number today? "))
+# if num % 2 == 0 :
+#     print("Even") 
+# else :
+#     print("Odd")
+
+
