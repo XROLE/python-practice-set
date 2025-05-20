bd65a068e9363d3c86d62a597959d9386fc9944f
xrole <xrolediamond@gmail.com>
2025-05-20 14:55:11 +0100

    solve finding largest number from inputs

---
+++ n_largest.py
@@ -0,0 +1,24 @@
+# ✅ 3. Find the Largest Number
+# Take 3 numbers as input and print the largest.
+# python
+# CopyEdit
+# Input: 10, 5, 20
+# Output: 20 is the largest
+
+# ==============================================>
+# solution
+
+num1 = float(input("Enter num1 "))
+num2 = float(input("Enter num2 "))
+num3 = float(input("Enter num3 "))
+
+largestNum = 0
+
+if num1 > num2 and num1 > num3:
+    largestNum = num1
+elif num2 > num1 and num2 > num3:
+    largestNum = num2
+else: 
+    largestNum = num3
+
+print(f"{largestNum} is the largest number")
--- simple_calculator.py
+++ simple_calculator.py
@@ -8,25 +8,25 @@
 
 # ======================================>
 # solution
-num1 = int(input("Enter first number \n"))
-operand = input("Enter an operand e.g (+, -, *, /)  \n")
-num2 = int(input("Enter second number \n"))
+# num1 = int(input("Enter first number \n"))
+# operand = input("Enter an operand e.g (+, -, *, /)  \n")
+# num2 = int(input("Enter second number \n"))
 
 
-if operand == '=':
-    result = num1 + num2
-elif operand == '*':
-    result = num1 * num2
-elif operand == '-':
-    result = num1 - num2
-elif operand == '/':
-    if num2 != 0:
-        result = num1 / num1
-    else :
-        result = "Error: can not be divided by zero"
-else:
-    result = "Invalid operator"
+# if operand == '=':
+#     result = num1 + num2
+# elif operand == '*':
+#     result = num1 * num2
+# elif operand == '-':
+#     result = num1 - num2
+# elif operand == '/':
+#     if num2 != 0:
+#         result = num1 / num1
+#     else :
+#         result = "Error: can not be divided by zero"
+# else:
+#     result = "Invalid operator"
 
 
 
-print(f"{num1} {operand} {num2} = {result}")
+# print(f"{num1} {operand} {num2} = {result}")
