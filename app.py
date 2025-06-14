03b66ca66a141679857148b42f157d8bb98be829
xrole <xrolediamond@gmail.com>
2025-06-14 07:47:47 +0100

    solve simple contact book problem

--- app.py
+++ app.py
@@ -1,13 +1,1 @@
 
-magicSentence = input("Make a sentence: \n")
-wordsFromSentence = magicSentence.split(" ")
-
-sentenceWordCount = {}
-
-for word in wordsFromSentence :
-    if word in sentenceWordCount :
-        sentenceWordCount[word] += 1
-    else : 
-        sentenceWordCount[word] = 1
-
-print(sentenceWordCount) 
---
+++ simple_contact_book.py
@@ -0,0 +1,19 @@
+phoneBook  = {}
+stop = False
+
+while stop == False:
+    queryString = input("Input query parameter : \n")
+    queries = queryString.split(" ")
+    print(f"Your Query String is : {queryString} \n")
+    
+    if(queries[0] == "Add") :
+        phoneBook[queries[1]] = queries[2]
+        print(f"Phone book updated:  {phoneBook} \n")
+    elif queries[0] == "Get" :
+        print(f"This is {queries[1]}'s contact : {phoneBook[queries[1]]}")
+    else :
+        print(f"{queries[0]} is not a valid queries \n")
+        stop = True
+        
+    
+ 
